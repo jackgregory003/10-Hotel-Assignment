4aa7abe5bdbdddaabb5bf8b2ff8d18a8d6693fc8
Jack Gregory <4balancingrocks@gmail.com>
2025-04-06 13:48:11 -0600

    first commit

---
+++ data/hotels.js
@@ -0,0 +1,52 @@
+export const hotels = [
+      {
+        "photo": "../images/hotel01-small.webp",
+        "name": "Grand Sunset Resort",
+        "phone": "555-123-4567",
+        "address": ["123 Ocean View Dr", "Miami, FL 33101"]
+      },
+      {
+        "photo": "../images/hotel02-small.webp",
+        "name": "Ocean View Hotel",
+        "phone": "555-234-5678",
+        "address": ["456 Alpine Rd", "Aspen, CO 81611"]
+      },
+      {
+        "photo": "../images/hotel03-small.webp",
+        "name": "City Lights Hotel",
+        "phone": "555-345-6789",
+        "address": ["789 Downtown Ave", "New York, NY 10001"]
+      },
+      {
+        "photo": "../images/hotel04-small.webp",
+        "name": "Pheonix Retreat",
+        "phone": "555-456-7890",
+        "address": ["159 Sand Dune Way", "Phoenix, AZ 85001"]
+      },
+      {
+        "photo": "../images/hotel05-small.webp",
+        "name": "Lakeside Haven",
+        "phone": "555-567-8901",
+        "address": ["753 Lakeview Dr", "Minneapolis, MN 55401"]
+      },
+      {
+        "photo": "../images/hotel06-small.webp",
+        "name": "Skyline Suites",
+        "phone": "555-678-9012",
+        "address": ["321 Pinecone Ln", "Portland, OR 97201"]
+      },
+      {
+        "photo": "../images/hotel07-small.webp",
+        "name": "Golden Gate Suites",
+        "phone": "555-789-0123",
+        "address": ["951 Bay Bridge Blvd", "San Francisco, CA 94101"]
+      },
+      {
+        "photo": "../images/hotel08-small.webp",
+        "name": "Boston Inn",
+        "phone": "555-890-1234",
+        "address": ["852 Royal Ct", "Boston, MA 02101"]
+      },
+    ];
+
+  
